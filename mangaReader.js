a7836552dcadbb74ef7971e7a81baf8f43edecee
Jaycen <saitakusei@gmail.com>
2023-04-17 20:12:54 +0900

    add next btn for manhuagui

--- mangaReader.js
+++ mangaReader.js
@@ -5,6 +5,7 @@
 // @description  use side button to go to next page or manga site
 // @author       You
 // @match        https://www.colamanhua.com/*
+// @match        https://www.manhuagui.com/*
 // @icon         https://www.colamanhua.com/favicon.png
 // @grant        none
 // ==/UserScript==
@@ -20,7 +21,7 @@
       'mouseup',
       'mousemove',
     ];
-  
+
     events.forEach(event => {
       console.log('remove event', event);
       document.removeEventListener(event, () => {});
@@ -28,7 +29,7 @@
   }
 
   removeExistingEvent();
- 
+
   const mangaReader = (next=false) => {
     console.log(e);
     let arr = window.location.href.split('/');
@@ -38,7 +39,7 @@
 
     let left = num - 1;
     let right = num + 1;
-    next ? window.location.href = url + '/' + right + '.html' : window.location.href = url + '/' + left + '.html';    
+    next ? window.location.href = url + '/' + right + '.html' : window.location.href = url + '/' + left + '.html';
   };
 
   document.addEventListener("mouseup", (e) => {
@@ -54,7 +55,18 @@
         break;
       default:
         if (e.button == 4) {
-          mangaReader(true);
+          console.log(window.location.hostname)
+          switch (window.location.hostname.toString()) {
+            case 'www.manhuagui.com':
+              try {
+                const nextBtn = document.getElementsByClassName("btn-red nextC");
+                nextBtn[0].click()
+              }catch(e) {};
+              break;
+            case 'www.colamanhua.com':
+              mangaReader(true);
+              break;
+          }
         }
     }
   });
